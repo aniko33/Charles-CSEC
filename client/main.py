87c92044525bd752a22319e8965be0bfb24c836f
Aniko <76649588+aniko33@users.noreply.github.com>
2022-12-23 18:24:29 +0100

    Update main.py

--- client/main.py
+++ client/main.py
@@ -8,72 +8,71 @@ import threading
 
 from rich import print
 from rich.console import Console
-from rich.tree import Tree 
+from rich.tree import Tree
 from rich.align import Align
 
+global buffer
+
+# Vars
 console = Console()
 s = socket.socket()
-
-#var
 buffer = 1024
 
+
+"""
+def cls:
+    Clear terminal screen
+"""
+
+
 def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    if os.name == 'nt':
+        os.system('cls')
 
-class Chat:
-    def __init__(self, priv_key, pub_key) -> None:
-        self.priv_key = priv_key
-        self.pub_key = pub_key
-    def send(self, msg:str):
-        s.send(rsa.encrypt(msg.encode(), self.pub_key))
-    def recv(self, buffer: int):
-        msg = s.recv(buffer)
-        return rsa.decrypt(msg, self.priv_key).decode()
+    else:
+        os.system("clear")
 
-class Main:
-    def __init__(self) -> None:
-        pass
 
-    class UI:
-            def __init__(self) -> None:
-                pass
-            def random_color(self):
-                #color list: https://rich.readthedocs.io/en/stable/appendix/colors.html
-                colors = ["red", "blue", "cyan", "yellow", "magenta", "green", "purple", "violet", "gold"]
-                
-                #random choice from array
-                color = random.choice(colors)
-                return color
-            def banner(self):
-                print(Align("""[purple]
-  __________________
- / ___/ __/ __/ ___/
-/ /___\ \/ _// /__  
-\___/___/___/\___/
-""","center"))
-            def get_server(self):
-                server_ip = console.input("[b]Insert server address[/b] [purple]>>[/purple] ")
-                server_port =  int(console.input("[b]Insert server port[/b] [purple]>>[/purple] "))
-                return server_ip, server_port
+class API:
+    """
+    class Chat:
+        arg: private_key, public_key
 
-            def get_username(self):
-                username = console.input("[b]Insert your username[/b] [purple]>>[/purple] ")
-                #random color set
-                color = self.random_color()
-                username_styled = f"<[{color}]{username}[/{color}]>"
-                return username, username_styled
+        def send (message: str):
+            Allows you to send an encrypted message to the server
 
-            def start(self):
-                self.banner()
-                ip, port = self.get_server()
-                username, username_styled = self.get_username()
-                return ip, port, username, username_styled
+        def recv (buffer: int):
+            Receives a message from the encrypted server
+
+    class Load_keys:
+        arg: private_key, public_key
+
+        def private:
+            Load the private key
+
+        def public:
+            Load the public key
+
+        def load_all:
+            Load the private and public key
+    """
+    class Chat:
+        def __init__(self, priv_key, pub_key) -> None:
+            self.priv_key = priv_key
+            self.pub_key = pub_key
+
+        def send(self, msg: str):
+            s.send(rsa.encrypt(msg.encode(), self.pub_key))
+
+        def recv(self, buffer: int):
+            msg = s.recv(buffer)
+            return rsa.decrypt(msg, self.priv_key).decode()
 
     class Load_keys:
         def __init__(self, pub_key, priv_key) -> None:
             self.pub_key = pub_key
             self.priv_key = priv_key
-        
+
         def private(self):
             return rsa.PrivateKey.load_pkcs1(self.priv_key)
 
@@ -81,8 +80,141 @@ class Main:
             return rsa.PublicKey.load_pkcs1(self.pub_key)
 
         def load_all(self):
-            return rsa.PrivateKey.load_pkcs1(self.priv_key), rsa.PublicKey.load_pkcs1(self.pub_key)
+            return rsa.PrivateKey.load_pkcs1(
+                self.priv_key), rsa.PublicKey.load_pkcs1(self.pub_key)
 
+
+class UI:
+    """
+    def random_color:
+        It will take a random color and be entered as the username color
+
+    def banner:
+        The ASCII banner that will be centered
+
+    def get_server:
+        Get the server ip and port
+
+    def get_username:
+        Get the username from the input will send it to the server
+
+    def run:
+        Where the code will start
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def random_color(self):
+        # Color list:
+        # https://rich.readthedocs.io/en/stable/appendix/colors.html
+        colors = [
+            "red",
+            "blue",
+            "cyan",
+            "yellow",
+            "magenta",
+            "green",
+            "purple",
+            "violet",
+            "gold"]
+
+        # Random choice from array
+        color = random.choice(colors)
+        return color
+
+    def banner(self):
+        print(Align(r"""[purple]
+  _____  _____ ______ _____
+ / ____|/ ____|  ____/ ____|
+ | |    | (___ | |__ | |
+ | |     \___ \|  __|| |
+ | |____ ____) | |___| |____
+ \_____|_____/|______\_____|
+""", "center"))
+
+    def get_server(self):
+        server_ip = console.input(
+            "[b]Insert server address[/b] [purple]>>[/purple] ")
+        server_port = int(
+            console.input("[b]Insert server port[/b] [purple]>>[/purple] "))
+        return server_ip, server_port
+
+    def get_username(self):
+        username = console.input(
+            "[b]Insert your username[/b] [purple]>>[/purple] ")
+        # Random color set
+        color = self.random_color()
+        username_styled = f"<[{color}]{username}[/{color}]>"
+        return username, username_styled
+
+    def start(self):
+        self.banner()
+        ip, port = self.get_server()
+        username, username_styled = self.get_username()
+        return ip, port, username, username_styled
+
+
+class Chat:
+    """
+    class chat_api: RSA custom socket
+
+    var (str) username_syled: (<Username>) {
+        It is the username but compliant for sending via socket
+        making all the username available
+    }
+
+    fun receive: Receive messages via socket
+    fun write: Where the user will put the message, it will be encrypted
+    fun run: Where the code will start
+    """
+
+    def __init__(self, chat_api, username_styled) -> None:
+        self.chat_api = chat_api
+        self.username_styled = username_styled
+
+    def receive(self):
+        while True:
+            print(self.chat_api.recv(buffer))
+
+    def write(self):
+        while True:
+            msg = input()
+
+            # Remove line up
+            sys.stdout.write("\033[F")
+
+            if (len(msg.strip()) > 0):
+                self.chat_api.send(self.username_styled + " " + msg)
+                print("<[green][i]You[/i][/green]> " + msg)
+
+    def run(self):
+        receive_process = threading.Thread(target=self.receive)
+        receive_process.start()
+        write_process = threading.Thread(target=self.write)
+        write_process.start()
+
+
+class Main:
+    """
+    def connect:
+        It allows you to connect to the server, you will receive the RSA keys
+
+    def send_username:
+        Send yours username to server
+
+    def get_buffer:
+        Get from server buffer - RSA size and message size
+
+    def get_welcome_message:
+        Get welcome message from server
+
+    def run:
+        Where the code will start
+    """
+
+    def __init__(self) -> None:
+        pass
 
     def connect(self):
         while True:
@@ -93,70 +225,47 @@ class Main:
                 print("[red]ERROR[/red]: Connection refused")
                 UI = Main.UI()
                 self.ip, self.port = UI.get_server()
-                self.username = UI.get_username()
+                self.username, self.username_styled = UI.get_username()
             else:
                 break
-        
+
+        self.send_username(self.username)
+
         buffer = self.get_buffer()
 
-        #decompress keys with zlib
-        private_key = zlib.decompress(s.recv(buffer))
+        # Decompress keys with zlib
         public_key = zlib.decompress(s.recv(buffer))
-        
-        #key load
-        client_key = self.Load_keys(public_key, private_key)
-        self.private_key, self.public_key = client_key.load_all()
-        
-        #API
-        self.chat_sock = Chat(self.private_key, self.public_key)
+        private_key = zlib.decompress(s.recv(buffer))
 
-        #check username existing
-        self.check_username(self.username)
-        
+        # Key load
+        client_key = API.Load_keys(public_key, private_key)
+        self.private_key, self.public_key = client_key.load_all()
+
+        # API
+        self.chat_api = API.Chat(self.private_key, self.public_key)
+
+        # Welcome screen
         cls()
-        print(Align(self.chat_sock.recv(buffer),"center"))
-    
-    def check_username(self, username: str):
+        self.get_welcome_message()
+
+        chat = Chat(self.chat_api, self.username_styled)
+        chat.run()
+
+    def send_username(self, username: str):
         s.send(username.encode())
-        status_code = s.recv(buffer)
-        print(status_code)
-        status_code = status_code.decode()
-        if status_code == "False":
-            s.close()
-            print("[red]ERROR[/red]: This user exist")
-            quit()
 
     def get_buffer(self):
         return int(s.recv(buffer).decode())
 
-    def chat(self):
-        def receive():
-            while True:
-                print(self.chat_sock.recv(buffer))
-        def write():
-            while True:
-                msg = input()
-                
-                #remove input
-                sys.stdout.write("\033[F")
-                
-                if (len(msg.strip()) > 0):
-                    #switch - commands
-                    match msg:
-                        case _:
-                            self.chat_sock.send(self.username_styled+" "+msg)
-                            print("<[green][i]You[i][/green]> "+msg)
-        receive_process = threading.Thread(target=receive)
-        receive_process.start()
-        write_process = threading.Thread(target=write)
-        write_process.start()
+    def get_welcome_message(self):
+        print(Align(self.chat_api.recv(1024), "center"))
 
     def run(self):
-        UI = Main.UI()
-        self.ip, self.port, self.username, self.username_styled = UI.start()
+        ui = UI()
+        self.ip, self.port, self.username, self.username_styled = ui.start()
         self.connect()
-        self.chat()
-        
+
+
 if __name__ == "__main__":
-    main_start = Main()
-    main_start.run()
+    main = Main()
+    main.run()
